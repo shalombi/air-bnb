686351ed6f0660172a1efc2d8c5d58410bd81da3
Shalom biton <shalombiton555@gmail.com>
2023-02-16 19:05:59 +0200

    g

--- src/data/stay.js
+++ src/data/stay.js
@@ -50,6 +50,8 @@ const stays = [
   }
 ]
 
+
+
 const orders = [
   {
     "_id": "o1225",
--- src/views/house-details.vue
+++ src/views/house-details.vue
@@ -1,8 +1,37 @@
 
 
 <template>
-    DETAILS...
-{{ this.$store.getters.displaySortBy }}
+    <section class="container">
+        <h1 class="title">Villa SUER- Maviye açilan eşsiz bir villa sizlerle</h1>
+        <section class="flex justify-content">
+
+            <div class="left-sub-title flex">
+                <div class="flex">
+                    <div class="flex">
+                        <img class="star" src="../assets/imgs/svgs/star.svg" />
+                        <span class="rate">5.0</span>
+                    </div>
+
+                    <span>Superhost</span>
+                    <span class="location-txt">Kaş, Antalya, Turkey</span>
+                </div>
+            </div>
+            <!-- <section></section> -->
+
+
+            <div class="flex right-sub-title">
+                <div class="flex">
+                    <span>Share</span>
+                    <img class="row-up" src="../assets/imgs/svgs/up-row.svg" alt="">
+                </div>
+                <div class="flex">
+                    <span>Save</span>
+                    <img class="like" src="../assets/imgs/svgs/like.svg" alt="">
+                </div>
+
+            </div>
+        </section>
+</section>
 </template>
 
 
